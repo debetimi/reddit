69c8e5da04e5c999aba96478720dc0484fcfa597
Debetimi Okah <debetimi@gmail.com>
2014-05-15 08:49:02 -0700

    compress

--- src/reddit/compression.clj
+++ src/reddit/compression.clj
@@ -1,65 +1,12 @@
 (ns reddit.compression
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as s :refer [split]]))
 
-;; Question
-;; http://www.reddit.com/r/dailyprogrammer/comments/25clki/5122014_challenge_162_easy_novel_compression_pt_1/
+(def input-file "resources/story.txt")
 
-(def input-file "resources/reader.txt")
+(def dictionary (atom []))
+(def lookup (atom {}))
+(def compressions (atom []))
 
-(defn get-data 
-  "Read in data to a string, then return
-   vector of string split on spaces"
+(defn get-data
   []
-  (string/split (string/replace (slurp input-file) #"\n" " ") #"\s+"))
-
-(defn get-fn 
-  "Match the command to a function"
-  [cmd]
-  (condp re-matches cmd 
-    #"\d+!" (comp print string/upper-case)
-    #"\d+\^" (comp print string/capitalize)
-    #"\d+" (comp print string/lower-case)))
-
-(defn get-index 
-  [cmd]
-  "Extract the index from command and convert to integer"
-  (read-string (last (re-matches #"(\d+).*" cmd))))
-
-(defn set-and-print
-  "Set the boolean for printing a space on the next iteration
-   and call your print function on provided word"
-  [printfn word add-space bool]
-  (reset! add-space bool)
-  (printfn word))
-
-(defn run []
-  (let [data (get-data) 
-        num-words (-> data first read-string)
-        words (-> data rest vec (subvec 0 num-words))
-        commands (-> data rest vec (subvec num-words))
-        i (atom 0)
-        add-space? (atom false)
-        continue? (atom true)]
-    (while @continue? 
-      (let [cmd (get commands @i)]
-        (condp re-matches cmd
-          #"[eE]" (swap! continue? not)
-          #"-" (set-and-print print "-" add-space? false) 
-          #"[\.\!\,\?\;\:]" (print cmd)
-          #"[rR]" (set-and-print println "" add-space? false)
-          (let [printfn (get-fn cmd)
-                idx (get-index cmd)
-                word (get words idx)]
-            (if @add-space? (print " "))
-            (set-and-print printfn word add-space? true))))
-      (swap! i inc))))
-
-
-(comment 
-  I do not like them in a house.
-  I do not like them with a mouse.
-  I do not like them here or there.
-  I do not like them anywhere.
-  I do not like green eggs and ham.
-  I do not like them, Sam-I-am.
-  nil)
+  (split (s/replace (slurp input-file) #"\n" " \n ") #"[^\S+\n]"))
---
+++ src/reddit/decompression.clj
@@ -0,0 +1,85 @@
+(ns reddit.decompression
+  (:require [clojure.string :as s :refer [upper-case capitalize
+                                          lower-case split join]]))
+
+;; Question
+;; http://www.reddit.com/r/dailyprogrammer/comments/25clki/5122014_challenge_162_easy_novel_compression_pt_1/
+
+(def input-file "resources/reader.txt")
+(def out-file "resources/decompressed.txt")
+
+(def out (atom []))
+
+(defn add
+  [info]
+  (swap! out conj info))
+
+(defn get-data 
+  "Read in data to a string, then return
+   vector of string split on spaces"
+  []
+  (split (s/replace (slurp input-file) #"\n" " ") #"\s+"))
+
+(defn parse
+  "Return a subvector of data from the specificed
+   indicies"
+  [data start & more]
+  (let [end (first more)
+        sub (if end #(subvec % start end) #(subvec % start))]
+    (-> data rest vec sub)))
+
+
+(defn cmd->fn 
+  "Match the command to a function"
+  [cmd]
+  (condp re-matches cmd 
+    #"\d+!" (comp add upper-case)
+    #"\d+\^" (comp add capitalize)
+    #"\d+" (comp add lower-case)))
+
+(defn get-index 
+  [cmd]
+  "Extract the index from command and convert to integer"
+  (read-string (last (re-matches #"(\d+).*" cmd))))
+
+(defn update
+  "Set the boolean for adding a space on the next iteration
+   and call your add function on provided word"
+  [addfn word add-space bool]
+  (reset! add-space bool)
+  (addfn word))
+
+(defn decompress 
+  []
+  (let [data (get-data) 
+        num-words (-> data first read-string)
+        words (parse data 0 num-words)
+        commands (parse data num-words)
+        i (atom 0)
+        space? (atom false)
+        continue? (atom true)]
+    (while @continue? 
+      (let [cmd (get commands @i)]
+        (condp re-matches cmd
+          #"[eE]" (swap! continue? not)
+          #"-" (update add "-" space? false) 
+          #"[\.\!\,\?\;\:]" (add cmd)
+          #"[rR]" (update add "\n" space? false)
+          (let [f (cmd->fn cmd)
+                n (get-index cmd)
+                w (get words n)]
+            (if @space? (add " "))
+            (update f w space? true))))
+      (swap! i inc))
+    (spit out-file (join @out))))
+
+
+(comment 
+  I do not like them in a house.
+  I do not like them with a mouse.
+  I do not like them here or there.
+  I do not like them anywhere.
+  I do not like green eggs and ham.
+  I do not like them, Sam-I-am.
+  nil)
+
